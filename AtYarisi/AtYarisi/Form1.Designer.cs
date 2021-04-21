c5c423168caed6d3b31c0f77a19fd0bcfbc95a9e
Sadigzade Mikayil <75858136+mikayil1551@users.noreply.github.com>
2021-04-21 12:54:37 +0300

    Add files via upload

---
+++ AtYarisi/AtYarisi/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace AtYarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.atRuzgarGulu1 = new System.Windows.Forms.PictureBox();
+            this.atYagiz2 = new System.Windows.Forms.PictureBox();
+            this.atPoyraz3 = new System.Windows.Forms.PictureBox();
+            this.atGolgeYeli4 = new System.Windows.Forms.PictureBox();
+            this.atAlber5 = new System.Windows.Forms.PictureBox();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.lblFinish = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.atRuzgarGulu1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atYagiz2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atPoyraz3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atGolgeYeli4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atAlber5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // atRuzgarGulu1
+            // 
+            this.atRuzgarGulu1.Image = ((System.Drawing.Image)(resources.GetObject("atRuzgarGulu1.Image")));
+            this.atRuzgarGulu1.Location = new System.Drawing.Point(2, 39);
+            this.atRuzgarGulu1.Name = "atRuzgarGulu1";
+            this.atRuzgarGulu1.Size = new System.Drawing.Size(118, 77);
+            this.atRuzgarGulu1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atRuzgarGulu1.TabIndex = 0;
+            this.atRuzgarGulu1.TabStop = false;
+            // 
+            // atYagiz2
+            // 
+            this.atYagiz2.BackColor = System.Drawing.SystemColors.GrayText;
+            this.atYagiz2.Image = ((System.Drawing.Image)(resources.GetObject("atYagiz2.Image")));
+            this.atYagiz2.Location = new System.Drawing.Point(2, 122);
+            this.atYagiz2.Name = "atYagiz2";
+            this.atYagiz2.Size = new System.Drawing.Size(118, 77);
+            this.atYagiz2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atYagiz2.TabIndex = 0;
+            this.atYagiz2.TabStop = false;
+            // 
+            // atPoyraz3
+            // 
+            this.atPoyraz3.Image = ((System.Drawing.Image)(resources.GetObject("atPoyraz3.Image")));
+            this.atPoyraz3.Location = new System.Drawing.Point(2, 205);
+            this.atPoyraz3.Name = "atPoyraz3";
+            this.atPoyraz3.Size = new System.Drawing.Size(118, 77);
+            this.atPoyraz3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atPoyraz3.TabIndex = 0;
+            this.atPoyraz3.TabStop = false;
+            // 
+            // atGolgeYeli4
+            // 
+            this.atGolgeYeli4.Image = ((System.Drawing.Image)(resources.GetObject("atGolgeYeli4.Image")));
+            this.atGolgeYeli4.Location = new System.Drawing.Point(2, 288);
+            this.atGolgeYeli4.Name = "atGolgeYeli4";
+            this.atGolgeYeli4.Size = new System.Drawing.Size(118, 77);
+            this.atGolgeYeli4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atGolgeYeli4.TabIndex = 0;
+            this.atGolgeYeli4.TabStop = false;
+            // 
+            // atAlber5
+            // 
+            this.atAlber5.BackColor = System.Drawing.SystemColors.GrayText;
+            this.atAlber5.Image = ((System.Drawing.Image)(resources.GetObject("atAlber5.Image")));
+            this.atAlber5.Location = new System.Drawing.Point(2, 371);
+            this.atAlber5.Name = "atAlber5";
+            this.atAlber5.Size = new System.Drawing.Size(118, 77);
+            this.atAlber5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atAlber5.TabIndex = 0;
+            this.atAlber5.TabStop = false;
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(2, 10);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(118, 23);
+            this.btnBaslat.TabIndex = 1;
+            this.btnBaslat.Text = "Baslat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // lblFinish
+            // 
+            this.lblFinish.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFinish.BackColor = System.Drawing.Color.Red;
+            this.lblFinish.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFinish.Location = new System.Drawing.Point(786, 1);
+            this.lblFinish.Name = "lblFinish";
+            this.lblFinish.Size = new System.Drawing.Size(14, 447);
+            this.lblFinish.TabIndex = 2;
+            this.lblFinish.Text = "FINISH";
+            this.lblFinish.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 35;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 450);
+            this.Controls.Add(this.lblFinish);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.atAlber5);
+            this.Controls.Add(this.atGolgeYeli4);
+            this.Controls.Add(this.atPoyraz3);
+            this.Controls.Add(this.atYagiz2);
+            this.Controls.Add(this.atRuzgarGulu1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.atRuzgarGulu1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atYagiz2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atPoyraz3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atGolgeYeli4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atAlber5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox atRuzgarGulu1;
+        private System.Windows.Forms.PictureBox atYagiz2;
+        private System.Windows.Forms.PictureBox atPoyraz3;
+        private System.Windows.Forms.PictureBox atGolgeYeli4;
+        private System.Windows.Forms.PictureBox atAlber5;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Label lblFinish;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ AtYarisi/AtYarisi/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtYarisi
+{
+    public partial class Form1 : Form
+    {
+        public object MainForm { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            AtDurumDegistir(true);
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(102,102,102);
+            AtDurumDegistir(false);
+
+        }
+        void AtDurumDegistir(bool durum,PictureBox at)
+        {
+            atAlber5.Enabled = atGolgeYeli4.Enabled = atYagiz2.Enabled
+                = atRuzgarGulu1.Enabled = atPoyraz3.Enabled = durum;
+            at.Enabled = false;
+        }
+        void AtDurumDegistir(bool durum)
+        {
+            atAlber5.Enabled = atGolgeYeli4.Enabled = atYagiz2.Enabled
+               = atRuzgarGulu1.Enabled = atPoyraz3.Enabled = durum;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            atYagiz2.Left += rnd.Next(1, 5);
+            atPoyraz3.Left += rnd.Next(1, 5);
+            atRuzgarGulu1.Left += rnd.Next(1, 5);
+            atGolgeYeli4.Left += rnd.Next(1, 5);
+            atAlber5.Left += rnd.Next(1, 5);
+
+            KimKazandi(atRuzgarGulu1);
+            KimKazandi(atGolgeYeli4);
+            KimKazandi(atPoyraz3);
+            KimKazandi(atAlber5);
+            KimKazandi(atYagiz2);
+           
+        }
+        void KimKazandi (PictureBox kazanan)
+        {
+            if (kazanan.Right - 17 >= lblFinish.Left)
+            {
+                timer1.Stop();
+                AtDurumDegistir(false, kazanan);
+                MessageBox.Show(kazanan.Name);
+            }
+        }
+    }
+}
